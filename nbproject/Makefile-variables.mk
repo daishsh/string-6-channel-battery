2afcc503e356243948e283e493b28bd07f8aa79b
imjustadog <imjustadog@outlook.com>
2017-08-25 11:13:59 +0800

    led 1s

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=string-6-channel-batery.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/string-6-channel-batery.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=string-6-channel-batery.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/string-6-channel-batery.tar
